8753d9fa7f946a25d5765c728986a7a541c54f6c
platonio00 <125078889+platonio00@users.noreply.github.com>
2023-02-10 20:48:34 +0300

    Add files via upload

---
+++ HellowWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellowWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("HellowWorld");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
